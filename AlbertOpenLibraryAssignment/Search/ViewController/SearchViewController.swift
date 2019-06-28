6e314a3585c8635940b75ee34ca4471cbfaa711c
Nick <nscondry@gmail.com>
2019-06-27 17:41:55 -0700

    Implemented initial RestManager; communicating with openlibrary

--- AlbertOpenLibraryAssignment/Search/ViewController/SearchViewController.swift
+++ AlbertOpenLibraryAssignment/Search/ViewController/SearchViewController.swift
@@ -10,10 +10,13 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    private var rest: RestManager!
     private var searchView: SearchView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        rest = RestManager()
 
         searchView = SearchView(frame: UIScreen.main.bounds)
         self.view = searchView
@@ -21,7 +24,23 @@ class SearchViewController: UIViewController {
         // navBar setup
         self.navigationItem.title = "Search"
         
-        print("searchVC setup")
+        getBooks()
+    }
+    
+    // testing REST calls
+    func getBooks() {
+        print("requesting...")
+        guard let url = URL(string: "https://openlibrary.org/search.json?q=the+lord+of+the+rings") else { return }
+        
+        rest.makeRequest(toURL: url, withHttpMethod: .get) { results in
+            if let data = results.data {
+                print(data)
+//                let decoder = JSONDecoder()
+//                decoder.keyDecodingStrategy = .convertFromSnakeCase
+//                guard let userData = try? decoder.decode(UserData.self, from: data) else { return }
+//                print(userData.description)
+            }
+        }
     }
     
 
